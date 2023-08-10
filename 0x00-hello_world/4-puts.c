a775576d996b959117ccf762fea7b7941d57d1a2
ahmad-atif <360000f@gmail.com>
2023-08-10 09:02:06 -0700

    new

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+* main - This program to use puts to print string to stdout stream
+* Return:0
+*/
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
