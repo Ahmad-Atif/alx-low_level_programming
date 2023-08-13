1088d9c7f62b45e852cae914651e3b7239c347fe
ahmad-atif <360000f@gmail.com>
2023-08-13 05:47:23 -0700

    ne

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -3,16 +3,22 @@
 #include <time.h>
 /**
  * main - Entry point
+ *
  * Description: Prints the lowercase alphabet followed by a new line.
- * Return: Always 0 (Success)*/
+ *
+ * Return: Always 0 (Success)
+ */
 int main(void)
 {
-  char letter = 'a';
-  while (letter <= 'z')
-  {
-    putchar(letter);
-    letter++;
-  }
+    char letter = 'a';
+
+    while (letter <= 'z')
+    {
+        putchar(letter);
+        letter++;
+    }
+
     putchar('\n');
+
     return (0);
 }
