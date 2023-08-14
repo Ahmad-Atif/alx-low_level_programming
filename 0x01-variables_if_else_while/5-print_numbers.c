f750d501c97ad7ae316856847c2b76382710565b
ahmad-atif <360000f@gmail.com>
2023-08-14 05:21:52 -0700

    nne

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdlib.h>
+#include <time.h>
+#include <stdio.h>
+
+/**
+* main - print digits of base10 system
+* Return:0
+*/
+int main(void)
+{
+	int i;
+
+for (i = 0; i < 10; i++)
+   {
+       printf ("%d\n" ,i);
+   {
+       return (0);
+} 
